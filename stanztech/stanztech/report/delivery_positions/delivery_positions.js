fd2139256dc97f9e75406d769072307e8b68bc47
lasalesi <larsericmueller@gmail.com>
2023-11-28 22:52:52 +0100

    improvements delivery positions

--- stanztech/stanztech/report/delivery_positions/delivery_positions.js
+++ stanztech/stanztech/report/delivery_positions/delivery_positions.js
@@ -15,6 +15,51 @@ frappe.query_reports["Delivery Positions"] = {
             "fieldtype": "Date",
             "default": frappe.datetime.add_days(frappe.datetime.get_today(), +14),
             "reqd": 1
+        },
+        {
+            "fieldname":"order_type",
+            "label": __("Typ"),
+            "fieldtype": "Select",
+            "options": "\nexterne\ninterne"
         }
-    ]
+    ],
+    "onload": (report) => {
+        if (!locals.double_click_handler) {
+            locals.double_click_handler = true;
+            
+            // add event listener for double clicks
+            cur_page.container.addEventListener("dblclick", function(event) {
+                console.log(event);
+                var row = event.delegatedTarget.getAttribute("data-row-index");
+                var column = event.delegatedTarget.getAttribute("data-col-index");
+                frappe.prompt([
+                        {
+                            'fieldname': 'information', 
+                            'fieldtype': 'Data', 
+                            'label': __('Information'), 
+                            'default': frappe.query_report.data[row].information,
+                            'reqd': 1
+                        }  
+                    ],
+                    function(values){
+                        frappe.call({
+                            'method': 'frappe.client.set_value',
+                            'args': {
+                                'doctype': 'Sales Order',
+                                'name': frappe.query_report.data[row].sales_order,
+                                'fieldname': 'information',
+                                'value': values.information
+                            },
+                            'callback': function() {
+                                frappe.query_report.refresh();
+                            }
+                        });
+
+                    },
+                    'Information',
+                    'OK'
+                );
+            });
+        }
+    }
 };
--- stanztech/stanztech/report/delivery_positions/delivery_positions.py
+++ stanztech/stanztech/report/delivery_positions/delivery_positions.py
@@ -5,8 +5,9 @@ from __future__ import unicode_literals
 import frappe
 from frappe import _
 import ast
-from datetime import datetime, timedelta
-from frappe.utils import cint
+#from datetime import datetime, timedelta, combine
+import datetime
+from frappe.utils import cint, get_url_to_form
 
 def execute(filters=None):
     columns = get_columns()
@@ -18,19 +19,23 @@ def get_columns():
         {"label": _("SO Item Date"), "fieldname": "so_item_date", "fieldtype": "Date", "width": 80},
         {"label": _("Item"), "fieldname": "item_code", "fieldtype": "Link", "options": "Item", "width": 130},
         {"label": _("Description"), "fieldname": "description", "fieldtype": "Text Editor", "width": 200},
-        {"label": _("Sales Order"), "fieldname": "sales_order", "fieldtype": "Link", "options": "Sales Order", "width": 120},
-        # {"label": _("Customer"), "fieldname": "customer", "fieldtype": "Link", "options": "Customer", "width": 40},        
+        #{"label": _("Sales Order"), "fieldname": "sales_order", "fieldtype": "Link", "options": "Sales Order", "width": 120},
+        #{"label": _("Customer"), "fieldname": "customer", "fieldtype": "Link", "options": "Customer", "width": 40},        
         {"label": _("Customer name"), "fieldname": "customer_name", "fieldtype": "Data", "width": 200},
-        {"label": _("SO Date"), "fieldname": "so_date", "fieldtype": "Date", "width": 80},
+        #{"label": _("SO Date"), "fieldname": "so_date", "fieldtype": "Date", "width": 80},
         #{"label": _("SO Delivery"), "fieldname": "so_delivery", "fieldtype": "Date", "width": 80},
         {"label": _("Reference"), "fieldname": "reference", "fieldtype": "Data", "width": 100},
         #{"label": _("Purchase"), "fieldname": "is_purchase_item", "fieldtype": "Check", "width": 20},
-        {"label": _("Purchase Order"), "fieldname": "purchase_order", "fieldtype": "Link", "options": "Purchase Order", "width": 130},
+        {"label": _(""), "fieldname": "indicator_purchase", "fieldtype": "Data", "width": 20},
+        #{"label": _("Purchase Order"), "fieldname": "purchase_order", "fieldtype": "Link", "options": "Purchase Order", "width": 130},
+        {"label": _("Purchase Receipt"), "fieldname": "purchase_receipt", "fieldtype": "Link", "options": "Purchase Receipt", "width": 130},
         {"label": _("Order Confirmation No"), "fieldname": "order_confirmation_no", "fieldtype": "Data", "width": 150},
+        {"label": _(""), "fieldname": "indicator_delivery", "fieldtype": "Data", "width": 20},
         {"label": _("Reqd By Date"), "fieldname": "po_item_date", "fieldtype": "Date", "width": 80},
         {"label": _("Qty"), "fieldname": "qty", "fieldtype": "Float", "width": 80},
         {"label": _("Qty delivered"), "fieldname": "qty_delivered", "fieldtype": "Float",  "width": 80},
         #{"label": _("Done"), "fieldname": "done", "fieldtype": "Check",  "width": 30},
+        {"label": _("Status"), "fieldname": "status", "fieldtype": "Data",  "width": 120},
         {"label": _("Information"), "fieldname": "information", "fieldtype": "Data",  "width": 450}
     ]
 
@@ -43,7 +48,13 @@ def get_data(filters):
     conditions = ""
     if 'from_date' in filters:
         conditions += " AND `tabSales Order`.`delivery_date` >= '{from_date}' ".format(from_date=filters['from_date'])
-        
+    
+    if 'order_type' in filters:
+        if filters.get('order_type') == "externe":
+            conditions += " AND `tabPurchase Order`.`order_confirmation_no` IS NOT NULL "
+        elif filters.get('order_type') == "interne":
+            conditions += " AND `tabPurchase Order`.`order_confirmation_no` IS NULL "
+            
     # prepare query
     sql_query = """SELECT
           `tabSales Order`.`name` AS `sales_order`,
@@ -56,12 +67,26 @@ def get_data(filters):
           `tabItem`.`description` AS `description`,
           `tabSales Order Item`.`item_code` AS `item_code`,
           `tabPurchase Order Item`.`parent` AS `purchase_order`,
+          (SELECT `tabPurchase Receipt Item`.`parent`
+           FROM `tabPurchase Receipt Item`
+           WHERE `tabPurchase Receipt Item`.`purchase_order` = `tabPurchase Order Item`.`parent`
+           LIMIT 1) AS `purchase_receipt`,
           `tabSales Order Item`.`delivery_date` AS `so_item_date`,
           `tabPurchase Order Item`.`schedule_date` AS `po_item_date`,
           `tabSales Order Item`.`qty` AS `qty`,
           `tabSales Order Item`.`delivered_qty` AS `qty_delivered`,
           `tabSales Order`.`information` AS `information`,
-          `tabPurchase Order`.`order_confirmation_no` AS `order_confirmation_no`
+          `tabPurchase Order`.`order_confirmation_no` AS `order_confirmation_no`,
+          (SELECT `tabWork Order`.`status`
+           FROM `tabWork Order`
+           WHERE `tabWork Order`.`sales_order` = `tabSales Order`.`name`
+             AND `tabWork Order`.`production_item` = `tabSales Order Item`.`item_code`
+             AND `tabWork Order`.`docstatus` < 2
+           LIMIT 1) AS `work_order_status`,
+          (SELECT `tabProject`.`status`
+           FROM `tabProject`
+           WHERE tabProject.`sales_order` = `tabSales Order`.`name`
+           LIMIT 1) AS `project_status`
         FROM `tabSales Order Item`
         LEFT JOIN `tabSales Order` ON `tabSales Order`.`name` = `tabSales Order Item`.`parent`
         LEFT JOIN `tabItem` ON `tabItem`.`item_code` = `tabSales Order Item`.`item_code` 
@@ -77,5 +102,52 @@ def get_data(filters):
       """.format(to_date=filters['to_date'], conditions=conditions)
     
     data = frappe.db.sql(sql_query, as_dict=True)
+    
+    now_ts = get_timestamp(datetime.datetime.now().date())
+    for d in data:
+        # purchase indicator
+        if not d['purchase_order']:
+            d['indicator_purchase'] = "<span style=\"color: silver; \">&#x2b24;</span>"
+        elif d['purchase_order'] and d['purchase_receipt']:
+            d['indicator_purchase'] = "<span style=\"color: green; \">&#x2b24;</span>"
+        else:
+            d['indicator_purchase'] = "<span style=\"color: red; \">&#x2b24;</span>"
         
+        # delivery indicator
+        if d['so_item_date']:
+            ts = get_timestamp(d['so_item_date'])
+            if (ts - now_ts) > (3 * 24 * 60 * 60):
+                d['indicator_delivery'] = "<span style=\"color: green; \">&#x2b24;</span>"
+            elif ts > now_ts:
+                d['indicator_delivery'] = "<span style=\"color: yellow; \">&#x2b24;</span>"
+            else:
+                d['indicator_delivery'] = "<span style=\"color: red; \">&#x2b24;</span>"
+        else:
+            d['indicator_delivery'] = "<span style=\"color: silver; \">&#x2b24;</span>"
+            
+        # rewrite reference as link to sales order
+        if d['sales_order']:
+            d['reference'] = "<a href=\"{0}\">{1}</a>".format(get_url_to_form("Sales Order", d['sales_order']), d['reference'])
+        
+        # rewrite order confirmation number to purchase order
+        if d['purchase_order']:
+            d['order_confirmation_no'] = "<a href=\"{0}\">{1}</a>".format(get_url_to_form("Purchase Order", d['purchase_order']), d['order_confirmation_no'])
+        
+        # consolidate status
+        d['status'] = d['work_order_status'] or d['project_status']
+        if d['status'] in ["Completed", "Closed"]:
+            d['status'] = "<span style=\"color: green; \">&#x2b24;</span> {0}".format(d['status'])
+        elif d['status'] in ["Not Started", "In Process", "Open"]:
+            d['status'] = "<span style=\"color: yellow; \">&#x2b24;</span> {0}".format(d['status'])
+        else:
+            d['status'] = "<span style=\"color: silver; \">&#x2b24;</span> {0}".format(d['status'] or "")
+            
     return data
+
+def get_timestamp(d):
+    if type(d) == str:
+        d = datetime.datetme.strptime(date, "%Y-%m-%d")
+    elif type(d) == datetime.date:
+        d = datetime.datetime.combine(d, datetime.time(0))
+        
+    return datetime.datetime.timestamp(d)
